d047625d221490596b0f059e6214bdf73b006b2b
Kavya <kavya.bhat@sap.com>
2019-09-19 14:55:11 +0530

    Fixing some build errors

--- libs/platform/src/lib/platform.module.ts
+++ libs/platform/src/lib/platform.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
 import { MenuModule } from './components/menu/menu.module';
-import { ActionModule } from './components/action/action.module';
-import { MenuComponent } from './components/menu/menu-comp/menu.component';
-import { MenuItemComponent } from './components/menu/menu-comp/menu-item.component';
-
 @NgModule({
     imports: [
         CommonModule,
         FundamentalNgxCoreModule
     ],
     exports: [
-        MenuModule,
-        ActionModule
+        MenuModule
     ]
 })
 export class FundamentalNgxPlatformModule {
