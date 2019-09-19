2bdc2770638daff620b62bf08b45efe5ba21a152
Kavya <kavya.bhat@sap.com>
2019-09-19 14:50:20 +0530

    Integrating Kevin's af-menu from shellbar to ngx
    Removed custom elements,
    added some css aligning with spec,
    made a MenuModule for using with demo app

--- libs/platform/src/lib/platform.module.ts
+++ libs/platform/src/lib/platform.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FundamentalNgxCoreModule } from '@fundamental-ngx/core';
+import { MenuModule } from './components/menu/menu.module';
+import { ActionModule } from './components/action/action.module';
+import { MenuComponent } from './components/menu/menu-comp/menu.component';
+import { MenuItemComponent } from './components/menu/menu-comp/menu-item.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FundamentalNgxCoreModule
+    ],
+    exports: [
+        MenuModule,
+        ActionModule
     ]
 })
 export class FundamentalNgxPlatformModule {
