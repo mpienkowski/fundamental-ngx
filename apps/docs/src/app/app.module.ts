0a624817a3b12f965f75c4adca7c2fef73c4e36e
Stefano Scalzo <stefano.scalzo@hotmail.com>
2019-09-25 15:00:29 -0400

    fixed links to work

--- apps/docs/src/app/app.module.ts
+++ apps/docs/src/app/app.module.ts
@@ -15,10 +15,7 @@ const routes: Routes = [
     declarations: [AppComponent],
     imports: [
         BrowserAnimationsModule,
-        RouterModule.forRoot(routes, {
-            scrollPositionRestoration: 'enabled',
-            anchorScrolling: 'enabled'
-        }),
+        RouterModule.forRoot(routes),
         HttpClientModule,
         MarkdownModule.forRoot({ loader: HttpClient }),
     ],
